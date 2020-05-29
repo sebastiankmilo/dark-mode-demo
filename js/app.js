0dbba353f74c571eefebb6875df6621e4f4b577a
Sebastian camilo cruz dussan <sebastiankmilo1996@gmail.com>
2020-05-29 13:13:54 -0500

    se termino de configurar las clases de estilo y el js para que se pudiera intercambiar entre el dark mode y el mono light

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
-const checkbox = document.querySelector('#checkbox')
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  checkbox.setAttribute('checked', true)
+const checkbox = document.querySelector("#checkbox");
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  checkbox.setAttribute("checked", true);
 }
 
-checkbox.addEventListener('change', function (event) {
+//configuro el toggle entre dark mode y light mode
+
+checkbox.addEventListener("change", function (event) {
+  console.log(this.checked);
   if (this.checked) {
-    document.body.classList.remove('is-light-mode')
-    document.body.classList.add('is-dark-mode')
+    console.log(this.checked);
+    document.body.classList.remove("is-light-mode");
+    document.body.classList.add("is-dark-mode");
   } else {
-    document.body.classList.remove('is-dark-mode')
-    document.body.classList.add('is-light-mode')
+    document.body.classList.remove("is-dark-mode");
+    document.body.classList.add("is-light-mode");
+    console.log(this.checked);
   }
-})
+});
+/*
+const checkbox = document.querySelector("#checkbox");
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  checkbox.setAttribute("checked", true);
+}
+
+checkbox.addEventListener("change", function (event) {
+  if (this.checked) {
+    document.body.classList.remove("is-light-mode");
+    document.body.classList.add("is-dark-mode");
+  } else {
+    document.body.classList.remove("is-dark-mode");
+    document.body.classList.add("is-light-mode");
+  }
+});
+*/
